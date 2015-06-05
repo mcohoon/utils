33fc217cf01f60eedb075f6fd9efbeed2993a350
jzinner <jzinner@gmail.com>
2015-06-04 17:31:12 -0700

    following PEP0008 style guides for file names

--- samples/Python/multiplication.py
+++
@@ -1,9 +0,0 @@
-def multiplication():
-    for m in range(1, 13):
-        singleline = []
-        for n in range(1, 13):
-            singleline.append(str(n*m))
-        print " ".join(singleline)
-
-multiplication()
-
